ceb595151615df8b7a087c3bad10826f3e24f1f1
GuestsUser <shoshun-nakazato-20h@stu.kbc.ac.jp>
2021-11-19 11:30:25 +0900

    いつも通りAsset/Scenesフォルダ内のBuildフォルダをコピーしてTestにリネームし作業を始めてほしい

---
+++ Assets/Script/Destroy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Destroy : MonoBehaviour
+{
+    
+    public Text scoreText;
+    public AudioClip clip;
+    private int score;
+    void Start()
+    {
+        score = 0;
+    }
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.CompareTag("Ball"))
+        {
+            // Instantiate(particleObject, this.transform.position, Quaternion.identity);
+            //Destroy(gameObject);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position); 
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position); 
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position); 
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            score = score + 1;
+
+
+        }
+      }
+    }
---
+++ Assets/Script/KoRider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoRider : MonoBehaviour
+{
+    [SerializeField] private bool is_trigger;
+    [SerializeField] private Vector3 center;
+    [SerializeField] private Vector3 size;
+
+    public float bounce;
+    [SerializeField] private BoundCombo bounce_combine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //p_material.bounciness;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
