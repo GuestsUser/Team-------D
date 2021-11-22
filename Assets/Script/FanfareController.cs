e0a8d60bcdadc5c226ce3ac209947c6ebfcf16f2
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2021-11-22 15:06:14 +0900

    effect処理　クリア時に動かせるようになったが、600F以上動く

---
+++ Assets/Script/FanfareController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanfareController : MonoBehaviour
+{
+    ParticleSystem fan;
+
+    void Start()
+    {
+        fan = this.GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (fan.isStopped)
+        {
+        }
+    }
+}
