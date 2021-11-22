ddfd0d4185cdda34bd53373e530c17294114f049
GuestsUser <shoshun-nakazato-20h@stu.kbc.ac.jp>
2021-11-22 14:01:38 +0900

    ボールとコインの判定サイズ修正、比較的軽快になった

---
+++ Assets/Script/FloorFollowRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//付けたオブジェクトの回転を床の回転と同期する
+public class FloorFollowRotation : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;//床オブジェクト
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = panel.transform.rotation;
+    }
+}
