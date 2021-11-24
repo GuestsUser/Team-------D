54ace14ad1f3a49d28dfdc25dd42170f352da290
GuestsUser <shoshun-nakazato-20h@stu.kbc.ac.jp>
2021-11-24 15:31:25 +0900

    エフェクトが床に接地、色設定が残ってたので色調整復活

---
+++ Assets/Script/EffectTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectTrigger : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem smoke; //smokeに床に接地したら起動したいパーティクルを入れてOnTriggerEnterで起動する奴
+    private bool flg=true;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.tag=="Ground"&&flg) {
+            smoke.gameObject.SetActive(true);
+            flg = false;
+        }
+    }
+}
