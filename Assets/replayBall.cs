39bda3494dcd12d7e2576b5946bf92d3c8e6fc5d
yoshi <d>
2021-12-01 09:27:04 +0900

    no message

---
+++ Assets/replayBall.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class replayBall: MonoBehaviour
+{
+    [SerializeField]
+   
+    private GameObject REplay;
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        GameObject playerObj = GameObject.Find(REplay.name);
+
+        // playerObjが存在していない場合
+        if (playerObj == null)
+        {
+            // playerPrefabから新しくGameObjectを作成
+            GameObject newPlayerObj = Instantiate(REplay);
+
+            newPlayerObj.name = REplay.name;
+
+        }
+    }
+}
