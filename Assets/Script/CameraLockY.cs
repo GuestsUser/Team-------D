f00526613c826bee8202c119da966ad098e87f45
4696neko <asuma-nakamura-20h@stu.kbc.ac.jp>
2021-12-01 10:23:02 +0900

    カメラ処理　カメラにわずかに残っていた荒ぶりを減らした

--- Assets/Script/CameraLockY.cs
+++ Assets/Script/CameraLockY.cs
@@ -8,14 +8,19 @@ public class CameraLockY : CinemachineExtension
 {
     [Tooltip("カメラのY座標を固定する値")]
     public float m_YPosition = 10;
+    public float m_Rotation = 0;
 
     protected override void PostPipelineStageCallback( CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
     {
         if(stage == CinemachineCore.Stage.Body)
         {
             var pos = state.RawPosition;
+            var rot = state.RawOrientation;
             pos.y = m_YPosition;
+            rot.y = m_Rotation;
+            rot.z = m_Rotation;
             state.RawPosition = pos;
+            state.RawOrientation = rot;
         }
     }
 }
