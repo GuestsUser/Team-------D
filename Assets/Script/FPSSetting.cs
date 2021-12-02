086c87fbecdbf8dfbab744baa396491486c1e5ca
GuestsUser <shoshun-nakazato-20h@stu.kbc.ac.jp>
2021-12-02 15:34:59 +0900

    さよならUpdate表示……完成版

---
+++ Assets/Script/FPSSetting.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSSetting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {//fixedupdateの値を60に固定
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
